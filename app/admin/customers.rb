983ead3f9403ce19af209dd6e5ceb137b922cdce
Kenson Catibog <kensoncatibog23@gmail.com>
2017-11-07 02:45:55 +0000

    part 3

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Customer do
 
-  permit_params :full_name, :phone_number, :email_address, :image, :notes
+  permit_params :full_name, :phone_number, :email_address, :image, :notes, :province_id
 
 end
